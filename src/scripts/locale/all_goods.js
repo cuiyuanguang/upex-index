edf4ad838ef80e3fca497109d8e8dedbb2ad653c
LuoHuacheng <154224099@qq.com>
2018-07-31 16:25:21 +0800

    广告列表语言包

--- src/scripts/locale/all_goods.js
+++ src/scripts/locale/all_goods.js
@@ -2,5 +2,167 @@ var messages = {
   welcome: {
     zh: '欢迎光临',
     en: 'Welcome',
+    sa: '',
+  },
+  releaseAdvert: {
+    zh: '发布挂单',
+    en: 'Post pending orders',
+    sa: '',
+  },
+  notSatisfied: {
+    zh: '对市场价不满意',
+    en: 'Market price is not satisfied',
+    sa: '',
+  },
+  sell: {
+    zh: '卖出',
+    en: 'Sell',
+    sa: '',
+  },
+  buy: {
+    zh: '买入',
+    en: 'Buy',
+    sa: '',
+  },
+  seller: {
+    zh: '卖家',
+    en: 'Seller',
+    sa: '',
+  },
+  buyer: {
+    zh: '买家',
+    en: 'Buyer',
+    sa: '',
+  },
+  amount: {
+    zh: '数量',
+    en: 'Amount',
+    sa: '',
+  },
+  limit: {
+    zh: '限额',
+    en: 'Limit',
+    sa: '',
+  },
+  min2maxPurchase: {
+    zh: '最小-最大购买额',
+    en: 'min-purchase amount - max-purchase amount',
+    sa: '',
+  },
+  unitPrice: {
+    zh: '单价',
+    en: 'Unit Price',
+    sa: '',
+  },
+  payment: {
+    zh: '支付方式',
+    en: 'Payments',
+    sa: '',
+  },
+  operation: {
+    zh: '操作',
+    en: 'Operation',
+    sa: '',
+  },
+  currentPrice: {
+    zh: '当前市场价',
+    en: 'Current market price',
+    sa: '',
+    sa: '',
+  },
+  currentReleasePrice: {
+    zh: '当前发布价',
+    en: 'Current release price',
+    sa: '',
+  },
+  pricePlaceholder: {
+    zh: '请输入您的期望售价',
+    en: 'Enter your expected selling price',
+    sa: '',
+  },
+  quantity: {
+    zh: '数量',
+    en: 'Quantity',
+    sa: '',
+  },
+  balance: {
+    zh: '余额',
+    en: 'Balance',
+    sa: '',
+  },
+  amountPlaceholder: {
+    zh: '请输入您的出售数量',
+    en: 'Enter your expected sell quantity',
+    sa: '',
+  },
+  totalPrice: {
+    zh: '总价',
+    en: 'total price',
+    sa: '',
+  },
+  transactionLimit: {
+    zh: '单笔限额',
+    en: 'Single transaction limit',
+    sa: '',
+  },
+  deviationWarn: {
+    zh: '价格波动提示',
+    en: 'Price deviation prompt',
+    sa: '',
+  },
+  moreThan50Percent: {
+    zh: '挂单价格大于市价50%，可能给您带来损失！',
+    en: 'Release price is 50% more than market price, this would bring you loss',
+    sa: '',
+  },
+  lessThan50Percent: {
+    zh: '挂单价格小于市价50%，可能给您带来损失！',
+    en: 'Release price is 50% less than market price, this would bring you loss',
+    sa: '',
+  },
+  lessThanHighestPurchase: {
+    zh: '挂单价格低于市场最高买价，可能给您带来损失！',
+    en: 'Release price is less than the highest purchase price, this would bring you loss',
+    sa: '',
+  },
+  moreThanLowestPrice: {
+    zh: '挂单价格高于市场最低卖价，可能给您带来损失！',
+    en: 'Release price is more than the lowest price, this would bring you loss',
+    sa: '',
+  },
+  confirmRelease: {
+    zh: '确认发布',
+    en: 'Confirm release',
+    sa: '',
+  },
+  modifyNow: {
+    zh: '立即修改',
+    en: 'modify immediately',
+    sa: '',
+  },
+  moreSetting: {
+    zh: '更多设置',
+    en: 'more settings',
+    sa: '',
+  },
+  bankCard: {
+    zh: '银行卡',
+    en: 'bank card',
+    sa: '',
+  },
+  googleAuthCode: {
+    zh: '谷歌验证码',
+    en: 'Google verification code',
+    sa: '',
+  },
+  smsCode: {
+    zh: '短信验证码',
+    en: 'SMS verification',
+    sa: '',
+  },
+  backEdit: {
+    zh: '返回修改',
+    en: 'back to edit',
+    sa: '',
   },
 };
