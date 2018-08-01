4ad234b6c8cc39d13cdb9a575a36a2cbc9e6d7a1
LuoHuacheng <154224099@qq.com>
2018-08-01 16:44:31 +0800

    我的挂单国际化

--- src/scripts/locale/my_goods.js
+++ src/scripts/locale/my_goods.js
@@ -3,4 +3,68 @@ var messages = {
       zh: '进行中的订单',
       en: 'My pending orders',
     },
+    type: {
+      zh: '类型',
+      en: 'Pending order type',
+    },
+    amount: {
+      zh: '数量',
+      en: 'Total amount',
+    },
+    unitPrice: {
+      zh: '单价',
+      en: 'Unit price',
+    },
+    limit: {
+      zh: '限额',
+      en: 'Limit',
+    },
+    time: {
+      zh: '时间',
+      en: 'Time',
+    },
+    operation: {
+      zh: '操作',
+      en: 'Operation',
+    },
+    confirm: {
+      zh: '确认',
+      en: 'Confirm',
+    },
+    cancel: {
+      zh: '取消',
+      en: 'Cancel',
+    },
+    start: {
+      zh: '开始接单',
+      en: 'Start'
+    },
+    pause: {
+      zh: '暂停接单',
+      en: 'Pause'
+    },
+    thisOrder: {
+      zh: '此订单',
+      en: 'this order',
+    },
+    unremovable: {
+      zh: '暂时不能移除',
+      en: 'Temporarily unable to remove'
+    },
+    unremovableTips: {
+      zh: '进行中的订单处理之后才能下线',
+      en: 'Pending orders, after processing, you can go offline'
+    },
+    unremovableOperation: {
+      zh: '立即处理',
+      en: 'Immediate treatment',
+    },
+    removable: {
+      zh: '移除订单',
+      en: 'Remove order'
+    },
+    removableTips: {
+      zh: '确认移除订单',
+      en: 'Confirm remove this order',
+    },
 };
