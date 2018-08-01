2283f6705b1dd76a1820b386c16f0a62c409c865
LuoHuacheng <154224099@qq.com>
2018-08-01 16:44:52 +0800

    我的订单国际化

--- src/scripts/locale/my_order.js
+++ src/scripts/locale/my_order.js
@@ -3,4 +3,68 @@ var messages = {
     zh: '我的订单',
     en: 'My Orders',
   },
+  orderNo: {
+    zh: '订单编号',
+    en: 'Order Number',
+  },
+  tradingType: {
+    zh: '交易类型',
+    en: 'Transaction Type',
+  },
+  tradingAmount: {
+    zh: '交易额',
+    en: 'Transaction Amount',
+  },
+  unitPrice: {
+    zh: '单价',
+    en: 'Unit Price',
+  },
+  totalPrice: {
+    zh: '总价',
+    en: 'Total Price',
+  },
+  time: {
+    zh: '时间',
+    en: 'Time',
+  },
+  status: {
+    zh: '状态',
+    en: 'Status',
+  },
+  tradingObject: {
+    zh: '交易对象',
+    en: 'Trading Partner',
+  },
+  toBePaid: {
+    zh: '待支付',
+    en: 'to be paid',
+  },
+  alreadyPaid: {
+    zh: '已支付',
+    en: 'already paid',
+  },
+  orderDone: {
+    zh: '交易完成',
+    en: 'order complete',
+  },
+  orderCanceled: {
+    zh: '交易取消',
+    en: 'order canceled',
+  },
+  appeal:{
+    zh: '申诉',
+    en: 'appeal',
+  },
+  transfering: {
+    zh: '转账中',
+    en: 'transfering',
+  },
+  orderAbnormal: {
+    zh: '订单异常',
+    en: 'order abnormal',
+  },
+  view: {
+    zh: '查看',
+    en: 'view',
+  },
 };
--- src/scripts/my_order.js
+++ src/scripts/my_order.js
@@ -6,7 +6,7 @@ var i18n = new VueI18n({
 });
 
 var myOrder = new Vue({
-  el: "#app",
+  el: '#app',
   i18n: i18n,
   components: {
     oHeader: o_header,
@@ -16,20 +16,20 @@ var myOrder = new Vue({
     pageSize: 10,
     current: 1,
     userInfo: {
-      uid: window.localStorage.getItem("uid"),
+      uid: window.localStorage.getItem('uid'),
     },
     uid: '',
     list: [],
     total: 0,
   },
   methods: {
-    getMyOrder: function (page) {
+    getMyOrder: function(page) {
       var that = this;
       var data = {
         pageSize: this.pageSize,
         pageNum: page || 1,
-      }
-      get("api/personOrders", data).then(function (res) {
+      };
+      get('api/personOrders', data).then(function(res) {
         if (res.success) {
           that.list = res.data.data.rsts;
           that.current = page;
@@ -37,39 +37,39 @@ var myOrder = new Vue({
         }
       });
     },
+    statusSemantics: function(v) {
+      // 订单状态 待支付1 已支付2 交易成功3 取消4 申诉5  打币中6  异常订单7
+      if (v == '1') {
+        return this.$t('toBePaid');
+      }
+      if (v == '2') {
+        return this.$t('alreadyPaid');
+      }
+      if (v == '3') {
+        return this.$t('orderDone');
+      }
+      if (v == '4') {
+        return this.$t('orderCanceled');
+      }
+      if (v == '5') {
+        return this.$t('appeal');
+      }
+      if (v == '6') {
+        return this.$t('transfering');
+      }
+      if (v == '7') {
+        return this.$t('orderAbnormal');
+      }
+    },
   },
   mounted() {
     this.getMyOrder();
   },
   filters: {
-    statusSemantics: function (v) {
-      // 订单状态 待支付1 已支付2 交易成功3 取消4 申诉5  打币中6  异常订单7
-      if (v == "1") {
-        return "to be paid";
-      }
-      if (v == "2") {
-        return "aleady paid";
-      }
-      if (v == "3") {
-        return "order complete";
-      }
-      if (v == "4") {
-        return "order canceled";
-      }
-      if (v == "5") {
-        return "申诉 ";
-      }
-      if (v == "6") {
-        return "打币中";
-      }
-      if (v == "7") {
-        return "Order Abnormal";
-      }
-    },
-    getDateFromMillionSeconds: function (v) {
+    getDateFromMillionSeconds: function(v) {
       var date = new Date(v);
       return date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate();
-    }
+    },
   },
   watch: {
     locale: function(newVal, oldVal) {
@@ -77,5 +77,5 @@ var myOrder = new Vue({
         this.$i18n.locale = newVal;
       }
     },
-  }
+  },
 });
