6c5eb80cf700844e1485c05860d19265c7b862bd
LuoHuacheng <154224099@qq.com>
2018-08-14 19:37:16 +0800

    语言文件替换

--- src/scripts/locale/my_order.js
+++ src/scripts/locale/my_order.js
@@ -2,81 +2,103 @@ var messages = {
   myOrder: {
     zh: '我的订单',
     en: 'My Orders',
+    ar: 'طلباتي',
   },
   orderNo: {
     zh: '订单编号',
     en: 'Order Number',
+    ar: 'رقم الطلب',
   },
   tradingType: {
     zh: '交易类型',
     en: 'Transaction Type',
+    ar: 'نوع الصفقة',
   },
   sell: {
     zh: '卖出',
     en: 'Sell',
+    ar: 'بيع',
   },
   buy: {
     zh: '买入',
     en: 'Buy',
+    ar: 'شراء',
   },
   tradingAmount: {
     zh: '交易额',
     en: 'Transaction Amount',
+    ar: 'مبلغ الصفقة',
   },
   unitPrice: {
     zh: '单价',
     en: 'Unit Price',
+    ar: 'سعر الوحدة',
   },
   totalPrice: {
     zh: '总价',
     en: 'Total Price',
+    ar: 'السعر الاجمالي',
   },
   time: {
     zh: '时间',
     en: 'Time',
+    ar: 'الوقت',
   },
   status: {
     zh: '状态',
     en: 'Status',
+    ar: 'الحالة',
   },
+
   tradingObject: {
     zh: '交易对象',
-    en: 'Trading Partner',
+    en: 'transaction partner',
+    ar: 'شريك الصفقة',
   },
+
   toBePaid: {
     zh: '待支付',
-    en: 'to be paid',
+    en: 'waiting to pay',
+    ar: 'في انتظار الدفع',
   },
   alreadyPaid: {
     zh: '已支付',
-    en: 'already paid',
+    en: 'Already paid',
+    ar: 'تم الدفع',
   },
   orderDone: {
     zh: '交易完成',
-    en: 'order complete',
+    en: 'Transaction completed',
+    ar: 'تم الصفقة',
   },
   orderCanceled: {
     zh: '交易取消',
-    en: 'order canceled',
+    en: 'transaction canceled',
+    ar: 'ألغيت الصفقة',
   },
-  appeal:{
+  appeal: {
     zh: '申诉',
-    en: 'appeal',
+    en: 'Appeal',
+    ar: 'طلب شكوى',
   },
   transfering: {
     zh: '转账中',
     en: 'transfering',
+    ar: 'جاري التحويل',
   },
   orderExpired: {
     zh: '订单过期',
     en: 'order expired',
+    ar: 'انتهت صلاحية الطلب',
   },
   view: {
     zh: '查看',
     en: 'view',
+    ar: 'عرض',
   },
   noData: {
     zh: '暂无数据',
     en: 'No Data',
+    ar: 'لا توجد بيانات',
   },
 };
--- src/scripts/locale/wait_pay.js
+++ src/scripts/locale/wait_pay.js
@@ -2,145 +2,213 @@ var messages = {
   waitForBuyer: {
     zh: '等待买家付款',
     en: 'Waiting for buyer to pay',
+    ar: 'في انتظار دفع المشتري',
   },
   buyerPaid: {
     zh: '买家已支付',
-    en: 'Buyer has already paid',
+    en: 'Buyer has paid',
+    ar: 'تم دفع المشتري',
   },
   transactionSuccessful: {
     zh: '交易成功',
-    en: 'Transaction successful',
+    en: 'transaction succeeded',
+    ar: 'تم الصفقة',
   },
   receivePayment: {
     zh: '收到法币',
-    en: 'Receive payment',
+    en: ' currency received',
+    ar: 'تم استلام العملة',
   },
   orderSuccess: {
     zh: '下单成功',
-    en: 'Order successfully',
+    en: 'order succeeded',
+    ar: 'تم الطلب',
   },
   payAndConfirm: {
     zh: '确认支付',
     en: 'Confirm payment',
+    ar: 'تأكيد الدفع',
   },
   tradeDone: {
     zh: '交易完成',
-    en: 'Transaction complete',
+    en: 'Transaction completed',
+    ar: 'تم الصفقة',
   },
   hour: {
     zh: '小时',
-    en: 'hours',
+    en: 'hour',
+    ar: 'ساعة',
   },
   restTime: {
     zh: '剩余时间',
-    en: 'time left',
+    en: 'remaining time',
+    ar: 'الوقت المتبقي',
   },
   waitForTime: {
     zh: '交易截止时间',
-    en: 'Waiting for the buyer to pay',
+    en: 'transaction deadline',
+    ar: 'الموعد النهائي للصفقة',
   },
   orderInfo: {
     zh: '订单信息',
-    en: 'Order Information',
+    en: 'Order Informations',
+    ar: 'معلومات الطلب',
   },
   orderNo: {
     zh: '订单编号',
     en: 'Order Number',
+    ar: 'رقم الطلب',
   },
   totalPurchase: {
     zh: '购买总量',
     en: 'Total purchase',
+    ar: 'إجمالي الشراء',
   },
   totalPrice: {
     zh: '支付金额',
-    en: 'Total price',
+    en: 'Payment amount',
+    ar: 'مبلغ الدفع',
   },
   received: {
     zh: '收到',
     en: 'Received',
+    ar: 'تم الاستلام',
   },
   sold: {
     zh: '出售',
     en: 'sold',
+    ar: 'تم البيع',
   },
   timeOfArrival: {
     zh: '到账时间',
-    en: 'Time of arrival',
+    en: 'Time of arrival to the account',
+    ar: 'وقت الوصول الى الحساب',
   },
   buyInfo: {
     zh: '买家信息',
-    en: 'Buyer Information',
+    en: 'Buyer Informations',
+    ar: 'معلومات المشتري',
   },
   buyName: {
     zh: '买家昵称',
-    en: 'Buyer Name',
+    en: 'Buyer nickname',
+    ar: 'لقب المشتري',
   },
   phone: {
     zh: '手机号',
-    en: 'phone',
+    en: 'phone number',
+    ar: 'رقم الهاتف',
   },
   socialNumber: {
     zh: '社交账号',
     en: 'social number',
+    ar: 'الحساب الاجتماعي',
   },
   contactBuyer: {
     zh: '联系买家',
-    en: 'contact buyer',
+    en: 'Contact the buyer',
+    ar: 'الاتصال بالمشتري',
   },
   backToHome: {
-    zh: '返回首页',
+    zh: '返回主页',
     en: 'back to homepage',
+    ar: 'العودة إلى الصفحة الرئيسية',
   },
   helpTipsFirst: {
     zh: '到账时间受不可抗力因素如跨行，大额转账影响，且银行系统并不受买方或卖方控制，请耐心等待。',
-    en: 'The delay in arrival time due to force majeure factors such as cross-banking, large-scale transfer, and bank settlement is beyond the control of both the buyer and the seller. Please wait patiently.',
+    en:
+      'Due to cross-bank services or large amounts transfer or other reasons caused delay in arrival time,is out of  seller or buyer control, so please wait patiently',
+    ar: 'بسبب الخدمات المصرفية بين البنوك المختلفة أو دفع المبالغ الكبيرة أوعوامل أخرى تسبب في تأخير وقت وصول المبلغ الى الحساب، هو خارج عن نطاق سيطرة البائع والمشتري ، لذلك يرجى الانتظار بالصبر',
   },
   helpTipsSecond: {
     zh: '如当天取消订单次数达到三次，支付功能将会被锁定。',
-    en: 'If the order is cancelled three times that day, the purchase function will be locked',
+    en: 'If order cancellation reached three times in the same day, the payment function will be closed',
+    ar: 'إذا بلغ إلغاء الطلب ثلاث مرات في نفس اليوم ، فسيتم إغلاق وظيفة الدفع',
   },
   helpTipsThird: {
     zh: '如您24小时内未收到法币或法币金额不正确，请联系客服',
-    en: 'If you do not receive the money within 24 hours, or the amount does not match, please contact customer service.',
+    en:
+      'If you do not receive the currency or the amount of the currency is incorrect within 24 hours, please contact customer service',
+    ar: 'إذا لم يتم استلام العملة أو مبلغ العملة غير صحيح خلال 24 ساعة ، يرجى الاتصال بخدمة العملاء',
   },
   helpTipsFourth: {
     zh: '若订单超时未处理，客服将会介入协助交易。',
-    en: 'Order overtime is not confirmed, customer service will assist the transaction.',
+    en: 'If order timeout is not processed，the customer service will be involved in assisting to complete the transaction',
+    ar: 'إذا نفذ الوقت لم يتم معالجة الطلب ، سوف تتدخل خدمة العملاء للمساعدة في إتمام الصفقة',
   },
   cancel: {
     zh: '取消',
     en: 'cancel',
+    ar: 'إلغاء',
   },
   lastFourNumber: {
     zh: '支付卡号后四位为',
-    en: 'digits after remittance card number',
+    en: 'The last four numbers of the payment card are',
+    ar: 'اَخر اربعة أرقام من بطاقة الدفع هي',
   },
   payYou: {
     zh: '向您支付',
-    en: 'to pay you',
+    en: 'pay to you',
+    ar: 'تدفع لك',
   },
   confirmReceive: {
     zh: '确认收款',
-    en: 'confirm receipt?'
+    en: 'confirm receipt',
+    ar: 'تأكيد الاستلام',
   },
   receiveAll: {
     zh: '是否收到全额法币？',
-    en: 'whether to receive the full amount?'
+    en: 'Have you received the full amount of the currency?',
+    ar: 'هل استلمت المبلغ الكامل للعملة ؟',
   },
   notReceiveAndWait: {
     zh: '若未收到，请耐心等待。',
-    en: 'If you do not receive it yet, please wait patiently',
+    en: 'If not received, please wait patiently',
+    ar: 'إذا لم يتم الاستلام، يرجى الانتظار بصبر',
   },
   confirm: {
     zh: '确认',
     en: 'confirm',
+    ar: 'تأكيد',
   },
   orderExpired: {
     zh: '订单过期',
     en: 'order expired',
+    ar: 'انتهت صلاحية الطلب',
   },
   copied: {
     zh: '已复制',
     en: 'has been copied',
+    ar: 'تم النسخ',
+  },
+  orderExpiredTips: {
+    zh: '订单已过期，无法查看信息',
+    en: 'Order expired, unable to view information!',
+    ar: 'انتهت صلاحية الطلب ، غير قادر على عرض المعلومات',
+  },
+  orderCanceled: {
+    zh: '订单已取消',
+    en: 'order canceled',
+    ar: 'تم إلغاء الطلب',
+  },
+  orderCanceledTips: {
+    zh: '订单已取消，无法继续操作',
+    en: 'Order canceled, unable to operate!',
+    ar: 'أجل ألغيت ، لا مزيد من العملية',
+  },
+  reasonForFailure: {
+    zh: '失败原因',
+    en: 'reason for failure',
+    ar: 'سبب الفشل',
+  },
+  outOfTimeToPay: {
+    zh: '买家超时未付款',
+    en: 'Buyer has not paid for overtime',
+    ar: 'لم يدفع المشتري مقابل العمل الإضافي',
+  },
+  outOfTimeToConfirm: {
+    zh: '卖家超时未确认',
+    en: 'Seller has not confirmed for overtime',
+    ar: 'البائع انتهت مهلة غير مؤكدة',
   },
 };
