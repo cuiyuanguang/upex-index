6a0472eb00e05e8e4ab6a12c3455ca92e9f7d2ed
DESKTOP-E6UM9LC\duckycui <18629548769@163.com>
2018-08-29 11:22:25 +0800

    平台介绍样式及中文

--- src/scripts/locale/my_introduce.js
+++ src/scripts/locale/my_introduce.js
@@ -1,96 +1,96 @@
 const messages = {
   certify: {
-    zh: '',
+    zh: '首个获得巴林央行（CBB）金融认证！',
     en: 'The first who has obtained the Financial Certification from Central Bank of Bahrain',
     ar: 'أول من حصل على الرخصة المالية من قبل مصرف البحرين المركزي'
   },
   platOne: {
-    zh: '',
+    zh: '平台担保，放心交易',
     en: 'Platform is guaranteedrest assured in trading',
     ar: 'المنصة مضمونة ، أطمئن في التداول'
   },
   platTwo: {
-    zh: '',
+    zh: '您的资产安全，由平台保驾护航！',
     en: 'Your assets are protected by the platform',
     ar: 'الأصول الخاصة بك محمية من قبل المنصة'
   },
   btcOne: {
-    zh: '',
+    zh: 'Upex交易保险金 ≈ XXXXXBTC',
     en: 'Upex Transaction insurance amount≈ XXXXXBTC',
     ar: 'الأصول الخاصة بك محمية من قبل المنصة'
   },
   btcTwo : {
-    zh: '',
+    zh: '每笔交易，由平台为您购买一份交易保险',
     en: 'The platform will purchase you a transaction insurance for each transaction',
     ar: 'سوف ستشتري لك المنصة تأمين لكل صفقة'
   },
   btcBtnOne: {
-    zh: '',
+    zh: '查询保险金',
     en: 'Check the insurance amount',
     ar: 'تحقق من مبلغ التأمين'
   },
   btcBtnTwo: {
-    zh: '',
+    zh: '担保细则',
     en: 'Details of guarantee',
     ar: ' تفاصيل الضمان'
   },
   introFiveTitle: {
-    zh: '',
+    zh: '平台的特点',
     en: 'Platform characteristics',
     ar: 'خصائص المنصة'
   },
   fiveLOne: {
-    zh: '',
+    zh: '客服极速相应',
     en: 'Customer service extreme response',
     ar: 'الاستجابة السريعة من جانب خدمة العملاء'
   },
   fiveLTwo: {
-    zh: '',
+    zh: '7x24小时客服',
     en: '7x24 hours customer service',
     ar: 'خدمة العملاء 7x24 ساعة'
   },
   fiveMOne:{
-    zh: '',
+    zh: '充币/提币，全网最快',
     en: 'Deposit / withdraw currencies, the fastest on the whole network',
     ar: 'إيداع / سحب العملات ، الأسرع في الشبكة بأكملها'
   },
   fiveMTwo: {
-    zh: '',
+    zh: '全网最快网络确认',
     en: 'The fastest network confirmation on the whole network\n',
     ar: 'أسرع تأكيد على الشبكة بأكملها'
   },
   fiveROne: {
-    zh: '',
+    zh: '银行安全级别',
     en: 'Bank security level',
     ar: 'مستوى أمن البنك'
   },
   fiveRTwo: {
-    zh: '',
+    zh: '3级防护，冷热钱包物理隔离',
     en: '3 level protection, physical isolation between cold and hot wallet',
     ar: 'مستويات ، والعزل الفيزيائي بين المحفظة الساخنة والباردة'
   },
   sixTitle: {
-    zh: '',
+    zh: '我们在哪？',
     en: 'Where are we?',
     ar: 'اين نحن؟'
   },
   sixLOne:{
-    zh: '',
+    zh: '公司总部',
     en: 'Company Headquarters',
     ar: 'المقر الرئيسي للشركة'
   },
   sixLTwo:{
-    zh: '',
+    zh: 'Arcapita-巴林：坐标',
     en: 'Arcapita-Bahrain: coordinates',
     ar: 'أركابيتا-البحرين: الإحداثيات'
   },
   sixLThree: {
-    zh: '',
+    zh: '负责整体中东业务',
     en: 'Responsible for overall Middle East business',
     ar: 'مسؤولة عن كل  الأعمال التجارية في الشرق الأوسط '
   },
   sixMOne:{
-    zh: '',
+    zh: '运营中心',
     en: 'Operations Center',
     ar: 'مركز التشغيل'
   },
@@ -100,32 +100,32 @@ const messages = {
     ar: 'القاهرة - مصر: الإحداثيات'
   },
   sixMThree: {
-    zh: '',
+    zh: '开罗-埃及：坐标',
     en: '30 operations, customer service and translation',
     ar: '30 مشغل ، خدمة العملاء والترجمة'
   },
   sixROne:{
-    zh: '',
+    zh: '研发中心',
     en: 'R & D Center (Research and Development Center)',
     ar: 'مركز البحث والتطوير'
   },
   sixRTwo:{
-    zh: '',
+    zh: '中关村-中国北京：坐标',
     en: 'Zhongguancun - Beijing, China: Coordinates',
     ar: 'تشونغ قوان تسون - بكين ، الصين: الإحداثيات'
   },
   sixRThree: {
-    zh: '',
+    zh: '80名顶级区块链研发工程师团队',
     en: 'There are 80 top block chain research and development engineers team',
     ar: 'هناك فريق مكون من 80  مهندس باحث ومطور في مجال البلوك تشين'
   },
   lastTitle: {
-    zh: '',
+    zh: '免费注册，开始交易',
     en: 'Register for free and start trading',
     ar: 'سجل مجانا وابدأ التداول'
   },
   startTrade: {
-    zh: '',
+    zh: '开始交易',
     en: 'start trading',
     ar: 'ابدأ التداول'
   }
